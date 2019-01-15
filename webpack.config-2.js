31df3cedfa671152703c264e79cc6b964fb0924f
vijayst <vijayst@gmail.com>
2019-01-15 16:04:30 +0530

    webpack 4 react config

---
+++ webpack.config-2.js
@@ -0,0 +1,76 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: [
+        'react-hot-loader/patch',
+        'webpack/hot/only-dev-server',
+        './src/app.jsx'
+    ],
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    devtool: 'cheap-module-eval-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                enforce: 'pre',
+                loader: 'eslint-loader',
+                options: {
+                    emitWarning: true
+                },
+            },
+            {
+                test: /.jsx?$/,
+                loader: 'babel-loader',
+                include: path.resolve(__dirname, 'src'),
+                exclude: /node_modules/,
+                options: {
+                    presets: [['es2015', { modules: false }], 'react'],
+                    plugins: ['react-hot-loader/babel']
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: [{
+                    loader: 'style-loader'
+                }, {
+                    loader: 'css-loader'
+                }, {
+                    loader: 'sass-loader'
+                }]
+            },
+            {
+                test: /\.(jpg|png|svg)$/,
+                loader: 'file-loader',
+                options: {
+                    name: '[path][name].[ext]'
+                }
+            }
+        ]
+    },
+    resolve: {
+        modules: [
+            'node_modules',
+            path.resolve(__dirname, 'src')
+        ],
+        extensions: ['.js', '.jsx'],
+    },
+    plugins: [
+        new CopyWebpackPlugin([
+            { from: 'src/index.html' }
+        ]),
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        hot: true,
+        overlay: {
+            errors: true,
+            warnings: true,
+        }
+    }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,76 +1,30 @@
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 const path = require('path');
-const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: [
-        'react-hot-loader/patch',
-        'webpack/hot/only-dev-server',
-        './src/app.jsx'
-    ],
+    mode: 'development',
+    entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+        filename: 'bundle.js',
+        publicPath: '/'
     },
-    devtool: 'cheap-module-eval-source-map',
     module: {
         rules: [
-            {
-                test: /\.jsx?$/,
-                enforce: 'pre',
-                loader: 'eslint-loader',
-                options: {
-                    emitWarning: true
-                },
-            },
             {
                 test: /.jsx?$/,
                 loader: 'babel-loader',
                 include: path.resolve(__dirname, 'src'),
                 exclude: /node_modules/,
                 options: {
-                    presets: [['es2015', { modules: false }], 'react'],
-                    plugins: ['react-hot-loader/babel']
-                }
-            },
-            {
-                test: /\.scss$/,
-                use: [{
-                    loader: 'style-loader'
-                }, {
-                    loader: 'css-loader'
-                }, {
-                    loader: 'sass-loader'
-                }]
-            },
-            {
-                test: /\.(jpg|png|svg)$/,
-                loader: 'file-loader',
-                options: {
-                    name: '[path][name].[ext]'
+                    presets: ['@babel/preset-env', '@babel/preset-react']
                 }
             }
         ]
     },
-    resolve: {
-        modules: [
-            'node_modules',
-            path.resolve(__dirname, 'src')
-        ],
-        extensions: ['.js', '.jsx'],
-    },
     plugins: [
-        new CopyWebpackPlugin([
-            { from: 'src/index.html' }
-        ]),
-        new webpack.HotModuleReplacementPlugin()
-    ],
-    devServer: {
-        contentBase: path.join(__dirname, 'dist'),
-        hot: true,
-        overlay: {
-            errors: true,
-            warnings: true,
-        }
-    }
-}
+        new HtmlWebpackPlugin({
+            template: 'src/index.html'
+        })
+    ]
+};

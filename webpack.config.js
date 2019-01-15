6a3f1507b4f1d66de4a7534dde3e6bf44ec67031
vijayst <vijayst@gmail.com>
2019-01-15 17:16:52 +0530

    webpack dev build

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,14 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.js?$/,
+                enforce: 'pre',
+                loader: 'eslint-loader',
+                options: {
+                    emitWarning: true
+                },
+            },
+            {
                 test: /.jsx?$/,
                 loader: 'babel-loader',
                 include: path.resolve(__dirname, 'src'),
